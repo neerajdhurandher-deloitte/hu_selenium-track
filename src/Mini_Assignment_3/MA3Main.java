b342cd593acde2563965ebbbc9d4c8756dc7ae29
neerajdhurandher-deloitte <101085508+neerajdhurandher-deloitte@users.noreply.github.com>
2022-03-24 16:15:47 +0530

    Mini Assignment 3 scenario 4 complected.

--- src/Mini_Assignment_3/MA3Main.java
+++ src/Mini_Assignment_3/MA3Main.java
@@ -1,14 +1,28 @@
 package Mini_Assignment_3;
 
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import java.awt.AWTException;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 public class MA3Main {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, IOException, AWTException {
         System.setProperty("webdriver.chrome.driver","C:\\Users\\ndhurandher\\Downloads\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
@@ -16,7 +30,10 @@ public class MA3Main {
 //        frameAction(driver);
 //        dragDrop(driver);
 //        alertAction(driver);
-        dragDropJS(driver);
+//        dragDropJS(driver);
+        ticketBooking(driver);
+
+//        driver.quit();
 
 
     }
@@ -91,14 +108,90 @@ public class MA3Main {
         Thread.sleep(2000);
 
         WebElement from =driver.findElement(By.xpath("//div[@id='column-a']"));
-//
         WebElement to =driver.findElement(By.xpath("//div[@id='column-b']"));
-//
         Actions actions = new Actions(driver);
         actions.clickAndHold(from).moveToElement(to).release(from).perform();
         System.out.println("Drag & Drop performed by using JavascriptExecutor");
 
+    }
+
+    public static void ticketBooking(WebDriver driver) throws InterruptedException, IOException, AWTException {
+
+        driver.get("https://www.goibibo.com");
+        System.out.println("website opened");
+        Thread.sleep(2000);
+
+        driver.findElement(By.xpath("//span[text() = 'Round-trip']")).click();
+        System.out.println("Round trip clicked");
+        Thread.sleep(2000);
+
+        driver.findElement(By.xpath("//span[text() = 'From']")).click();
+        driver.findElement(By.cssSelector("input[type*='text']")).sendKeys("New York");
+        Thread.sleep(2000);
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("(//div[contains(@class,'sc-iAKWXU iyyKqe')]) ["+1+"]")).click();
+        Thread.sleep(2000);
+        System.out.println("From selected");
+
+        driver.findElement(By.cssSelector("input[type*='text']")).sendKeys("Seattle");
+        Thread.sleep(2000);
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("(//div[contains(@class,'sc-iAKWXU iyyKqe')]) ["+1+"]")).click();
+        Thread.sleep(2000);
+
+        System.out.println("To selected");
+
+
+        Thread.sleep(2000);
+        driver.findElement(By.cssSelector("span[class*='DayPicker-NavButton DayPicker-NavButton--next']")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.cssSelector("span[class*='DayPicker-NavButton DayPicker-NavButton--next']")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.cssSelector("span[class*='DayPicker-NavButton DayPicker-NavButton--next']")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//p[text() = '24']")).click();
+        System.out.println("Departure date  selected");
+
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//span[text() = 'Return']")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.cssSelector("span[class*='DayPicker-NavButton DayPicker-NavButton--next']")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//p[text() = '8']")).click();
+        System.out.println("Return date  selected");
+
+        Thread.sleep(2000);
+
+        takeScreenshot("flight booking");
+
+        System.out.println("Take Screenshot ");
+
+        driver.findElement(By.xpath("//span[text() = 'Travellers & Class']")).click();
+        Thread.sleep(2000);
+
+//
+        driver.findElement(By.xpath("//span[text() = 'SEARCH FLIGHTS']")).click();
+        System.out.println("Search for flight");
+
+        driver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
+
+        System.out.println("Cheapest flight :- "+ driver.findElement(By.xpath("(//div[contains(@class,'srp-card-uistyles__Price-sc-3flq99-17 gqEhhU alignItemsCenter dF fb lh1 padT5')]) ["+1+"]")).getText() );
+
+        driver.findElement(By.xpath("(//button[contains(@class,'srp-card-uistyles__BookButton-sc-3flq99-21 bgObmb dF justifyCenter alignItemsCenter txtUpper')]) ["+1+"]")).click();
+        System.out.println("Cheapest flight selected");
+        Thread.sleep(2000);
+
+        System.out.println("Booked");
+
 
     }
+
+    public static void takeScreenshot(String fileName) throws IOException, AWTException {
+        BufferedImage image = new Robot().createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+
+        ImageIO.write(image, "png", new File("C:\\Users\\ndhurandher\\Pictures\\Screenshorts\\"+fileName+".png"));
+
+    }
+
 
 }
